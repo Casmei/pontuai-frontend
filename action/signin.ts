d5af820a0aec6df1f5da0a0547576b3ff0ad7e5e
Péricles Pires <periclespires@protonmail.com>
2025-04-13 15:47:52 -0300

    remove old front

--- action/signin.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { logtoConfig } from "@/config/logto";
-import { signIn } from "@logto/next/server-actions";
-
-export async function signin() {
-  await signIn(logtoConfig);
-}
